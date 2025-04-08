3d01f539a9d5b31f0e306680fa2e24c58a5bc59e
pushkar-gr <gr.pushkar.7@gmail.com>
2025-04-08 13:22:20 +0530

    add: custom errors for torrent reader

---
+++ src/core/torrent/torrent_error.rs
@@ -0,0 +1,50 @@
+use bencode::{DecoderError, streaming::Error};
+use thiserror::Error;
+
+//custom error enum for reading torrent operations
+#[derive(Error, Debug)]
+pub enum ReadTorrentError {
+    //variant for decode errors with a display message
+    #[error("Decoder error: {0}")]
+    DecoderError(#[from] BDecoderError),
+
+    //variant for streaming errors with a display message
+    #[error("Streaming error: {0}")]
+    StreamingError(#[from] BStreamingError),
+}
+
+//wrapper struct for DecoderError
+#[derive(Debug)]
+pub struct BDecoderError(DecoderError);
+
+impl std::fmt::Display for BDecoderError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:?}", self.0)
+    }
+}
+
+impl std::error::Error for BDecoderError {}
+
+impl From<DecoderError> for BDecoderError {
+    fn from(err: DecoderError) -> Self {
+        BDecoderError(err)
+    }
+}
+
+//wrapper struct for streaming::Error
+#[derive(Debug)]
+pub struct BStreamingError(Error);
+
+impl std::fmt::Display for BStreamingError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:?}", self.0)
+    }
+}
+
+impl std::error::Error for BStreamingError {}
+
+impl From<Error> for BStreamingError {
+    fn from(err: Error) -> Self {
+        BStreamingError(err)
+    }
+}
