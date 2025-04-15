f8532798191d3540d2201fe4392422091dd86f01
pushkar-gr <gr.pushkar.7@gmail.com>
2025-04-15 12:52:18 +0530

    add: main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod core;
+mod util;
+
+use core::torrent::torrent::TorrentFile;
+
+use std::env;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let file_path = args[1].clone();
+    let torrent_file = TorrentFile::from_file(&Path::new(&file_path)).unwrap();
+    println!("{:?}", torrent_file);
 }
